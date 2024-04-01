5924f9ad881bbaaf54c8682154ca33e7815f17e7
José Luis Montero <jluismontero@gmail.com>
2024-04-01 21:07:05 +0200

    Añadir ventana principal,conexion, modulo clientes

---
+++ auxiliares.py
@@ -0,0 +1,57 @@
+from PySide6.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel
+
+
+class VentanaEmergenteBorrar(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Atención")
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.layout.addWidget(QLabel("¿Está seguro de la eliminación?"))
+
+        self.botones = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        self.botones.accepted.connect(self.accept)
+        self.botones.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.botones)
+        
+class VentanaEmergenteVacio(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Atención")
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.layout.addWidget(QLabel("Se debe introducir un nombre"))
+
+        self.botones = QDialogButtonBox(
+            QDialogButtonBox.Ok)
+
+        self.botones.accepted.connect(self.accept)
+        # self.botones.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.botones)
+    
+class VentanaEmergenteFechaIncorrecta(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Atención")
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.layout.addWidget(QLabel("La fecha de caducidad debe ser mayor que la fecha de entrada"))
+
+        self.botones = QDialogButtonBox(
+            QDialogButtonBox.Ok)
+
+        self.botones.accepted.connect(self.accept)
+        # self.botones.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.botones)
+
